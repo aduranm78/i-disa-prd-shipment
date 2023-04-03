ec6a9ebd6c453002cc037424b5c61975df4ff1d8
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2023-04-02 20:15:55 -0600

    DST Time Change Off

--- src/main/java/com/example/WmsParams.java
+++ src/main/java/com/example/WmsParams.java
@@ -21,10 +21,12 @@ public class WmsParams {
       	Calendar c = Calendar.getInstance();
       	c.setTimeZone(tz);
       	formatter.setTimeZone(tz);
-        c.setTime(now);
-        c.add(Calendar.SECOND, secondsAgo * -1);
-        startDate = c.getTime();
-        endDate = now;
+		c.setTime(now);
+		c.add(Calendar.SECOND, secondsAgo * -1);
+		endDate = c.getTime();
+		c.add(Calendar.SECOND, secondsAgo * -1);
+		startDate = c.getTime();
+		//endDate = now;
       	
       	dates.put("start", formatter.format(startDate));
         dates.put("end", formatter.format(endDate));
