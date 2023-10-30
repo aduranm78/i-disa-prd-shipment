a3e014fee62689095878ee5cb57484b3329e9502
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2023-10-30 07:58:00 -0600

    Cambio Horario Invierno

--- src/main/java/com/example/WmsParams.java
+++ src/main/java/com/example/WmsParams.java
@@ -15,18 +15,16 @@ public class WmsParams {
 		Date startDate;
 		Date endDate;
 		JSONObject dates = new JSONObject();
-		TimeZone tz = TimeZone.getTimeZone("America/Mexico_City");
+		TimeZone tz = TimeZone.getTimeZone("GMT-6:00");
 
 		Date now = new Date(System.currentTimeMillis());
       	Calendar c = Calendar.getInstance();
       	c.setTimeZone(tz);
       	formatter.setTimeZone(tz);
-		c.setTime(now);
-		c.add(Calendar.SECOND, secondsAgo * -1);
-		endDate = c.getTime();
-		c.add(Calendar.SECOND, secondsAgo * -1);
-		startDate = c.getTime();
-		//endDate = now;
+        c.setTime(now);
+        c.add(Calendar.SECOND, secondsAgo * -1);
+        startDate = c.getTime();
+        endDate = now;
       	
       	dates.put("start", formatter.format(startDate));
         dates.put("end", formatter.format(endDate));
